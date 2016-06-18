a52c169102d142ca802378dfd6cd3391b64c45bf
Austin Thesing <admin@designxdevelop.com>
2016-06-17 23:45:52 -0700

    Ran the migration

--- db/schema.rb
+++ db/schema.rb
@@ -46,6 +46,5 @@ ActiveRecord::Schema.define(version: 20160618044500) do
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
-  add_index "users", ["username"], name: "index_users_on_username", unique: true
 
 end
