ebc0ced103b4d2411ed45f0f35a82bb6e3c03b3c
Austin Thesing <admin@designxdevelop.com>
2016-06-21 13:00:10 -0700

    AppsController generated

---
+++ app/controllers/apps_controller.rb
@@ -0,0 +1,20 @@
+class AppsController < ApplicationController
+  def index
+    @apps = App.all
+  end
+
+  def show
+  end
+
+  def new
+  end
+
+  def create
+  end
+
+  def edit
+  end
+
+  def destroy
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,13 @@
 Rails.application.routes.draw do
+  # Apps Controller routes
+  resources :apps
+
   # remember to restart the server after the devise install
   devise_for :users
 
+  # Welcome Controler routes
+  get 'about' => 'welcome#about'
   root 'welcome#index'
-
-  get '/about' => 'welcome#about'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
