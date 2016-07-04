c18e3c1f0d3ddc86da2c1b7390b3626ac0584481
Austin Thesing <admin@designxdevelop.com>
2016-07-04 13:02:04 -0700

    Implemented API controller.. Seems to be erorring on curl test though

--- app/controllers/api/events_contoller.rb
+++ app/controllers/api/events_contoller.rb
@@ -1,14 +1,31 @@
 class API::EventsController < ApplicationController
+  before_action :set_access_control_headers
   skip_before_action :verify_authenticity_token
 
   def create
     registered_app = App.find_by(url: request.env['HTTP_ORIGIN'])
 
-    if registered_app? == false
+    unless registered_app
       render json: "Unregistered App", status: :unprocessable_entity
     else
-      render json: @event, status: :created
+      @event = registered_app.events.new(event_params)
+      if @event.save
+        render json: @event, status: :created
+      else
+        render json: {errors: @event.errors}, status: :unprocessable_entity
+      end
     end
+
+  end
+
+  def preflight
+    head 200
+  end
+
+  def set_access_control_headers
+    headers['Access-Control-Allow-Origin'] = '*'
+    headers['Access-Control-Allow-Origin'] = 'POST, GET, OPTIONS'
+    headers['Access-Control-Allow-Origin'] = 'Content-Type'
   end
 
   private
